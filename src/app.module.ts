c2ac0c28a43868061761b7f6a73f491fea9b93ea
nonkululeko victoria <2106761@students.wits.ac.za>
2021-09-15 11:40:20 +0200

    aa

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		// MongooseModule.forRoot("mongodb+srv://nolan__kgotso:Nolan123@phonebook.gu113.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true, }),
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://nonkululeko:<nonkululeko>@phonebook.xdpow.mongodb.net/phoneDatabase?retryWrites=true&w=majority", { autoCreate: true, }),
+		// MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
 	providers: [],
 })
 export class AppModule {}
+
+
+      
